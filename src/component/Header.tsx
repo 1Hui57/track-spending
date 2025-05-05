9a6324eda7410c41b0345bb8d477b84532dfedc5
1Hui57 <melody870507@gmail.com>
2025-05-06 03:50:45 +0800

    feat:建立header

---
+++ src/component/Header.tsx
@@ -0,0 +1,16 @@
+// Header
+import "../style/header.css"
+
+export default function Header() {
+
+
+    return (
+        <header>
+            <nav>
+                <div id="header" className="header">
+                    <h3>React 記帳程式</h3>
+                </div>
+            </nav>
+        </header>
+    );
+}
